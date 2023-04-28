41fad4858b542b4100759a75c324137a8605fa68
dennisfofie <dennisbeckham000@gmail.com>
2023-04-28 03:03:24 +0000

    task 5

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""
-Description - return sum of all the elements in a list by checking the type
-"""
+"""Type-annotated function sum_mixed_list"""
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[int, float]) -> float:
-    sum: float = 0.0
-    for i in mxd_lst:
-        sum += i
-    return sum
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Accepts a mixed list of integers and floats and returns
+    their sum as float"""
+    a: float = 0.0
+    for x in mxd_lst:
+        a += x
+    return a
