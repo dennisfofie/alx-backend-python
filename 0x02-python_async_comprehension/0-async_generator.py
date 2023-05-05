56a50c7e8d56f45ef460b63ee8945ad89b4de679
dennisfofie <dennisbeckham000@gmail.com>
2023-05-05 08:06:54 +0000

    fix typing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """coroutine that asynchronously loop 10 times"""
+from typing import List
 import random
 import asyncio
 
 
-async def async_generator() -> list[float]:
+async def async_generator() -> List[float]:
     """This generate random numbers while sleeping 1sec"""
     for _ in range(10):
         await asyncio.sleep(1)
