84510f40704b6b83c171ea155bd588e3b20a405c
dennisfofie <dennisbeckham000@gmail.com>
2023-05-05 07:50:03 +0000

    fix typing

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Describe a file that returns function"""
 from sort import sort_list
+from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Sort a list of float in ascending order"""
     results: list = []
     for _ in range(n):
