fa0e4553f432103b845757f766e0747df777d149
dennisfofie <dennisbeckham000@gmail.com>
2023-05-05 07:59:20 +0000

    fix typing

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Describe a file that returns function"""
-from sort import sort_list
 
 task_wait_random = __import__("3-tasks").wait_random
 
@@ -11,4 +10,4 @@ async def task_wait_n(n: int, max_delay: int) -> list[float]:
     for _ in range(n):
         result: float = await task_wait_random(max_delay)
         results.append(result)
-    return sort_list(results)
+    return sorted(results)
